a323fc4698dd870d761ad32835fd6417edd71716
Trikita73 <diachenkoandrii7@gmail.com>
2021-01-12 06:01:29 +0200

    JS

---
+++ js/common.js
@@ -0,0 +1,72 @@
+// 1.1 Переменные 
+// camelCase
+//var name ='Adnrii' // string 
+//const firstName = 'Andrey' 
+//const lastName = 'Diach' // number
+
+//name = 'Andrey'
+//age = 30
+//lastNae = Diachenko
+
+//const isProgrammer = true // boolean
+
+//const_private = 'private'
+//onst $ = 'some value'
+
+// const if = 'mket' // err
+//const withNum7 = '7'
+// const 5withNum = 7 //err
+
+//console.log(firstName)
+
+// 1.2 Мутирование
+
+//console.log('Имя Человека: ' + firstName)
+//console.log('Имя Человека: ' + ', а возраст человека:' + age)
+//console.log(age)
+//console.log(age.toString())
+
+//alert('Имя Человека: ' + firstName)
+
+
+//alert(firstName + ' ' + lastName)
+
+// 3 Операторы 
+//const currentYear = 2020
+//const birthYear = 1991
+
+//const age = currentYear - birthYear
+
+
+//alert("Hello")
+
+//let message = 'Hello2'
+//alert(message)
+
+
+// Переменные //
+
+//let admin, name;
+//name = "Djon";
+//admin = name;
+//alert(admin);
+
+//let age;
+//alert(age); // выведет "undefined"
+
+//let name = "Ilya";
+//alert( `hello ${1}` ); // ?
+//alert( `hello ${"name"}` ); // ?
+//alert( `hello ${name}` ); // ?
+
+//let isBoss = confirm("Ты здесь главный?");
+//alert( isBoss ); // true, если нажата OK
+
+//let age = prompt('Сколько тебе лет?', 100);
+//alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+
+//let = prompt("Сколько тебе лет!", "")
+ //alert (name)
+
+ //let name = prompt("Write ur name", "");
+ //alert (name);
