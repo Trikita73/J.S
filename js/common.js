84807deabfc5995b2b000426606e3fc24550bc0d
Trikita73 <diachenkoandrii7@gmail.com>
2021-01-31 05:53:15 +0200

    js

--- js/common.js
+++ js/common.js
@@ -98,3 +98,27 @@
  }
 
 */
+
+
+
+//if (age >= 14 && age <= 90);
+
+//if(!(age >==14 && age <==90))
+
+let login = prompt('Введите логин', '');
+
+if(login == 'Admin') {
+	let password = prompt('Введите пароль', '');
+	if(password == 'I am main') {
+		alert("Привет Админ");
+	}else if(password == '' || password == null) {
+		alert('Отмена');
+	}else {
+		alert('не верный пароль');
+	}
+}else if(login == '' || login == null) {
+	alert('Отмена');
+}else {
+	alert('не верный логин');
+}
+
