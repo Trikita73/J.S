4986e014cd7607dd3dac82b6f9318d579eb24bd8
Trikita73 <diachenkoandrii7@gmail.com>
2021-02-08 14:40:59 +0200

    switch

--- js/common.js
+++ js/common.js
@@ -238,3 +238,52 @@ nextPrime:for(i = 2; i <=n; i++) { // Для всех i...
 	alert (i); //простое число
 }
 */
+
+// Конструкция Switch
+
+/*
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+// Аналог Switch - if...else
+
+let browser = prompt('Введите название браузера:', '');
+
+if(browser == 'Edge') {
+	alert("You've got the Edge!");
+}else if(browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+	alert('Okay we support these browsers too');
+}else {
+	alert('We hope that this page looks ok!');
+}
+*/
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch(number) {
+	case 0:
+		alert('Вы ввели число 0');
+		break;
+	case 1:
+		alert('Вы ввели число 1');
+		break;
+	case 2:
+	case 3:
+		alert('Вы ввели число 2, а может и 3');
+		break;
+	default:
+		alert('Неверное число!')			
+}
