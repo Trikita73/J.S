400da34a637aca7b8719b6a933bbc4a1ad2354c5
Trikita73 <diachenkoandrii7@gmail.com>
2021-05-03 21:45:41 +0300

    basic task 2

---
+++ js/JS_basic_1.2.js
@@ -0,0 +1,11 @@
+let number = prompt("Write Number", '');
+
+if (number > 0) {
+       alert ("1")
+}else if (number < 0) {
+       alert ("- 1");
+}else if (number == 0) {
+        alert("0");
+}else {
+    alert('something wrong');
+}
